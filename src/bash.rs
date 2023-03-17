cdd62dd274eb5543faca2d31611ac7e4b2996d74
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-03-17 08:31:09 +0800

    added session feature for bash

--- src/bash.rs
+++ src/bash.rs
@@ -1,17 +1,33 @@
-use std::{fmt, process::Command};
+use std::{
+    fmt,
+    process::{Command, Output},
+};
 
 pub fn bash_exec(args: &str) -> Result<String, BashErr> {
-    let mut cmd = Command::new("bash");
-    cmd.args(["-c", args]);
+    let mut cmd = bash_common(args);
 
     let res = cmd.output().unwrap();
 
-    match (res.status.success(), res) {
-        (true, a) => Ok(String::from_utf8(a.stdout).unwrap().trim().to_string()),
-        (false, e) => Err(BashErr(String::from_utf8(e.stdout).unwrap())),
-    }
+    Ok(bash_out(res))
 }
 
+pub async fn bash_spawn(args: &str) {
+    bash_common(args).spawn().unwrap().wait().unwrap();
+}
+
+fn bash_out(input: Output) -> String {
+    let stdout = String::from_utf8(input.stdout).unwrap_or_default();
+
+    let sterr = String::from_utf8(input.stderr).unwrap_or_default();
+
+    format!("{}{}", stdout.trim(), sterr.trim())
+}
+fn bash_common(args: &str) -> Command {
+    //let mut cmd = Command::new("bash");
+    let mut cmd = Command::new("bash");
+    cmd.args(["-c", args]);
+    cmd
+}
 #[derive(Debug, Clone)]
 pub struct BashErr(String);
 
